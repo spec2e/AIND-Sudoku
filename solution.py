bc6419b9b24ca38eedd6317e92020a70bd39acc6
spec2e <speconsult@gmail.com>
2017-10-22 18:49:30 +0200

    diagonals solved

--- solution.py
+++ solution.py
@@ -18,7 +18,7 @@ boxes = cross(rows, cols)
 row_units = [cross(r, cols) for r in rows]
 column_units = [cross(rows, c) for c in cols]
 square_units = [cross(rs, cs) for rs in ('ABC','DEF','GHI') for cs in ('123','456','789')]
-unitlist = row_units + column_units + square_units
+unitlist = row_units + column_units + square_units + diagonals
 units = dict((s, [u for u in unitlist if s in u]) for s in boxes)
 peers = dict((s, set(sum(units[s],[]))-set([s])) for s in boxes)
 
