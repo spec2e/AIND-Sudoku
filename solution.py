550c0df83541e5a8572652a95e960819440c3d3e
spec2e <speconsult@gmail.com>
2017-10-22 21:24:23 +0200

    almost there...

--- solution.py
+++ solution.py
@@ -139,6 +139,9 @@ def reduce_puzzle(values):
         # Your code here: Use the Only Choice Strategy
         values = only_choice(values)
 
+        # naked twins
+        values = naked_twins(values)
+
         # Check how many boxes have a determined value, to compare
         solved_values_after = len([box for box in values.keys() if len(values[box]) == 1])
         # If no new values were added, stop the loop.
